e34d7e5e8de2355d565dd2fec374a5fcae80a1fc
Olddevbless <Olddevbless@users.noreply.github.com>
2022-07-26 23:03:47 +0300

    platform spawner, needs tuning on spawn location

---
+++ Assets/Scripts/PlatformSpawner.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformSpawner : MonoBehaviour
+{
+    
+    public GameObject platform;
+    public bool diceIsGrounded= false;
+
+    private void Update()
+    {
+
+    if (Input.GetMouseButtonDown(1)&& diceIsGrounded==false)
+        {
+            Debug.Log("spawning platform");
+            Instantiate(platform, Input.mousePosition, Quaternion.identity);
+        }
+        
+    }
+}
