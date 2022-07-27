f30acb402dfb62ce80071656d9048d2741831dae
Olddevbless <Olddevbless@users.noreply.github.com>
2022-07-27 15:00:43 +0300

    changed playerjump to be based on how long player holds space

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,18 +7,46 @@ public class PlayerMovement : MonoBehaviour
     public float horizontalInput;
     public int speed = 5;
     public GameObject dice;
+    public GameObject feetPos;
+    public float checkRadius;
+    public LayerMask whatIsGround;
     public bool diceIsGrounded;
+    private Rigidbody playerRB;
+    public float jumpingPower;
+    private float jumpTimeCounter;
+    public float jumpTime;
+    public bool isJumping;
+    public bool playerIsGrounded;
     void Start()
     {
-       
+        playerRB = gameObject.GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        
         diceIsGrounded = dice.GetComponent<DiceMovement>().diceIsGrounded;
         horizontalInput = Input.GetAxis("Horizontal");
-        if (horizontalInput>0.01 && diceIsGrounded==true)
+        Walking();
+        Jumping();
+        
+    }
+    private void OnCollisionStay(Collision collision)
+    {
+        playerIsGrounded = true;
+        
+    }
+    private void OnCollisionExit(Collision collision)
+    {
+        playerIsGrounded = false;
+    }
+
+
+
+    void Walking()
+    {
+        if (horizontalInput > 0.01 && diceIsGrounded == true)
         {
             transform.Translate(Vector3.right * speed * Time.deltaTime);
         }
@@ -27,4 +55,34 @@ public class PlayerMovement : MonoBehaviour
             transform.Translate(Vector3.left * speed * Time.deltaTime);
         }
     }
+    void Jumping ()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && diceIsGrounded == true && playerIsGrounded == true)
+        {
+            playerRB.AddForce(Vector3.up * jumpingPower, ForceMode.Impulse);
+            jumpTimeCounter = jumpTime;
+            isJumping = true;
+        }
+        if (Input.GetKey(KeyCode.Space)&& isJumping == true)
+        {
+            if (jumpTimeCounter>0)
+            {
+                playerRB.AddForce(Vector3.up * jumpingPower);
+                jumpTimeCounter -= Time.deltaTime;
+              
+            }
+            else
+            {
+                isJumping = false;
+            }
+            
+        }
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            isJumping = false;
+        }
+    }
+
+
+    
 }
