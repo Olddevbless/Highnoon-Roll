29b63a8b08ea8aae8362a04e295e9ea03a0b227a
Olddevbless <Olddevbless@users.noreply.github.com>
2022-07-28 19:44:02 +0300

    sorted out platform placement and rotation

--- Assets/Scripts/PlatformSpawner.cs
+++ Assets/Scripts/PlatformSpawner.cs
@@ -8,14 +8,19 @@ public class PlatformSpawner : MonoBehaviour
     public GameObject platform;
     public GameObject dice;
     public bool diceIsGrounded;
+    public Camera mainCamera;
+    Vector3 aim;
 
     private void Update()
     {
+        Vector3 mousePos = Input.mousePosition;
+        mousePos += Camera.main.transform.forward * 30f; // Make sure to add some "depth" to the screen point
+        aim = Camera.main.ScreenToWorldPoint(new Vector3(mousePos[0], mousePos[1], 15f));
         diceIsGrounded = dice.GetComponent<DiceMovement>().diceIsGrounded;
     if (Input.GetMouseButtonDown(1)&& diceIsGrounded==false)
         {
             Debug.Log("spawning platform");
-            Instantiate(platform, Input.mousePosition, Quaternion.identity);
+            Instantiate(platform, aim, platform.transform.rotation);
         }
         
     }
