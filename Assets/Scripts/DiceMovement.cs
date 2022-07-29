7fb31bc35f108a562049f65e13a2a874a184bb9c
Olddevbless <Olddevbless@users.noreply.github.com>
2022-07-30 02:01:39 +0300

    table mat, bouncieness and spin on dice

--- Assets/Scripts/DiceMovement.cs
+++ Assets/Scripts/DiceMovement.cs
@@ -5,9 +5,14 @@ using UnityEngine;
 public class DiceMovement : MonoBehaviour
 {
     public bool diceIsGrounded;
+    Rigidbody diceRB;
+
 
     private void Start()
     {
+        diceRB = gameObject.GetComponent<Rigidbody>();
+        diceRB.AddTorque(new Vector3(1,1,1), ForceMode.Impulse);
+        diceRB.AddForce(Vector3.right, ForceMode.Impulse);
         diceIsGrounded = false;
     }
     private void OnCollisionEnter(Collision collision)
