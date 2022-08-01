7796236fa27354cd95fcbf35375521ed5a01f56d
Olddevbless <Olddevbless@users.noreply.github.com>
2022-08-01 11:19:19 +0300

    added carriage navigation

--- Assets/Scripts/PlayerCarriageMovement.cs
+++ Assets/Scripts/PlayerCarriageMovement.cs
@@ -2,9 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.AI;
 
 public class PlayerCarriageMovement : MonoBehaviour
 {
+    NavMeshAgent agent;
     [Header("Buttons")]
     public GameObject[] locations;
     public Camera mainCamera;
@@ -12,21 +14,21 @@ public class PlayerCarriageMovement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        agent = GetComponent<NavMeshAgent>();
     }
 
     // Update is called once per frame
     void Update()
-    {
-        Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
-        if(Physics.Raycast(ray,out RaycastHit raycastHit))
-        {
-            cursorSphere.transform.position = raycastHit.point;
-        }
-        
+    { 
         if (Input.GetMouseButton(0))
         {
-            gameObject.transform.Translate(Vector3.MoveTowards(gameObject.transform.position, new Vector3(cursorSphere.transform.position.x,2,cursorSphere.transform.position.z), 10f*Time.deltaTime));
+            Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out RaycastHit raycastHit,Mathf.Infinity))
+            {
+                agent.SetDestination(raycastHit.point);
+                //gameObject.transform.Translate(Vector3.MoveTowards(gameObject.transform.position, raycastHit.point, 10f * Time.deltaTime));
+            }
+                
             //for (int i = 0; i < locations.Length; i++)
             //{
                 //gameObject.transform.Translate(Vector3.MoveTowards(gameObject.transform.position, locations[i].transform.position));
